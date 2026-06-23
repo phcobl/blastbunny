3b8da441af5dec06d3a0b9ca845028033b0828f6
Topicranger <krusti@fluffytech.xyz>
2026-06-23 18:36:02 +0200

    Titlebar funny

--- main.lua
+++ main.lua
@@ -19,7 +19,21 @@ function love.load()
         "This will be a super duper minefield!",
         "Where's my puppygirl?",
         "Men? Where are men?",
-        "Pipe bomb! So cool! I wonder what happens if I-"
+        "Pipe bomb! So cool! I wonder what happens if I-",
+        "Oh no! ...Anyway",
+        "You got mines on your field?",
+        "Ahe,,,! Mine sweeper,,,",
+        "Expies' gonna suffer",
+        "Caine! THIS is our adventure?!",
+        "I am become mine. Destroyer of fields.",
+        "Secret fields? Mine sweeper?",
+        "Get that knight grip on your shovel",
+        "Sweep sweep sweep sweep~!",
+        "I can't talk right now, I'm sweeping mines.",
+        "It's literally pseudo-random, you'll figure it out",
+        "I'm literally a mine, what's your excuse?",
+        "19 DOLLAR MINEFIELD PLOT. Who wants it? I'm giving it away.",
+        "You like sweeping mines don't you?",
     }
     local randi = love.math.random(1, #subTitles)
     love.window.setTitle("Blastbunny: " .. subTitles[randi])
